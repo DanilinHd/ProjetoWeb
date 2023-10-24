363424b4b3493480a8048e818c0f3a9a80b967e5
Danilo Freitas <danilofeitosa93@gmail.com>
2023-10-23 22:08:39 -0300

    alterando banco de dados

--- delete_customer.php
+++ delete_customer.php
@@ -6,7 +6,13 @@ $del_id = filter_input(INPUT_POST, 'del_id');
 if ($del_id && $_SERVER['REQUEST_METHOD'] == 'POST') 
 {
 
-	if($_SESSION['admin_type']==='super')		
+	if($_SESSION['admin_type']==='super'){
+        // 	$_SESSION['failure'] = "You don't have permission to perform this action";
+        // 	header('location: customers.php');
+        //     exit;
+    
+        }
+        $customer_id = $del_id;		
 
     $db = getDbInstance();
     $db->where('id', $customer_id);
